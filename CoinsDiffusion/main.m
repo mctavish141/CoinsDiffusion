71c90929ca783ee148c97973553f84c0276c4b12
mctavish141 <Serega@MacBook-Air.local>
2017-10-22 14:15:22 +0300

    Initial Commit

---
+++ CoinsDiffusion/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoinsDiffusion
+//
+//  Created by Serhii Kopach on 22.10.17.
+//  Copyright © 2017 Serhii Kopach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
