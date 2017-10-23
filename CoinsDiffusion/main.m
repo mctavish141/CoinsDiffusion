fd2d640747b5f1d8886e742e1f776a3c76385e76
mctavish141 <Serega@MacBook-Air.local>
2017-10-23 23:24:45 +0300

    Incorrect input values checking added

--- CoinsDiffusion/main.m
+++ CoinsDiffusion/main.m
@@ -13,41 +13,55 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
-        NSString *result = @"";
-        
-        int a;
-        scanf("%d", &a);
-        
-        while ((a >= 1) && (a <= 20)) {
-            NSMutableArray *countries = [[NSMutableArray alloc] init];
+        while (1) {
+            NSString *result = @"";
             
-            for (int i = 0; i < a; i++) {
-                char country[26];
+            int a;
+            scanf("%d", &a);
+            
+            while ((a >= 1) && (a <= 20)) {
+                NSMutableArray *countries = [[NSMutableArray alloc] init];
                 
-                int xl, yl, xh, yh;
+                for (int i = 0; i < a; i++) {
+                    char country[26];
+                    
+                    int xl, yl, xh, yh;
+                    
+                    scanf("%s %d %d %d %d", country, &xl, &yl, &xh, &yh);
+                    
+                    if ((xl < 1) || (xl > 10) || (yl < 1) || (yl > 10) || (xh < 1) || (xh > 10) || (yh < 1) || (yh > 10)) {
+                        NSLog(@"Input values are incorrect...");
+                        return 0;
+                    }
+                    
+                    NSDictionary *countryDictionary = @{@"name": [NSString stringWithUTF8String:country],
+                                                        @"xl": @(xl),
+                                                        @"yl": @(yl),
+                                                        @"xh": @(xh),
+                                                        @"yh": @(yh)};
+                    [countries addObject:countryDictionary];
+                }
                 
-                scanf("%s %d %d %d %d", country, &xl, &yl, &xh, &yh);
+                World *world = [[World alloc] init];
+                [world createWorldWithCountriesDicts:countries];
                 
-                NSDictionary *countryDictionary = @{@"name": [NSString stringWithUTF8String:country],
-                                                    @"xl": @(xl),
-                                                    @"yl": @(yl),
-                                                    @"xh": @(xh),
-                                                    @"yh": @(yh)};
-                [countries addObject:countryDictionary];
+                static int caseNumber = 0;
+                caseNumber += 1;
+                
+                result = [result stringByAppendingString:[NSString stringWithFormat:@"\nCase Number %d%@", caseNumber, world.result]];
+                
+                scanf("%d", &a);
             }
             
-            World *world = [[World alloc] init];
-            [world createWorldWithCountriesDicts:countries];
+            if (a != 0) {
+                NSLog(@"Input values are incorrect...");
+                return 0;
+            }
             
-            static int caseNumber = 0;
-            caseNumber += 1;
-            
-            result = [result stringByAppendingString:[NSString stringWithFormat:@"\nCase Number %d%@", caseNumber, world.result]];
-            
-            scanf("%d", &a);
+            if (result.length > 0) {
+                NSLog(@"%@", result);
+            }
         }
-        
-        NSLog(@"%@", result);
     }
     return 0;
 }
